2019d65b257d12924af142cf5c69eb1d54368741
Shureck <Sashalev200149@gmail.com>
2024-11-10 01:20:33 +0300

    fast_algos

--- main.py
+++ main.py
@@ -50,8 +50,7 @@ origins = [
     "http://localhost:8080",
     "http://localhost:3000",
     "http://62.109.26.235:8180",
-    "http://62.109.26.235:3000",
-    "*"
+    "http://62.109.26.235:3000"
 ]
 
 app.add_middleware(
